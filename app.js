dc012cc3228ff3f7e08b285aa05a7bb0b78eba72
Yash1Hingu <yash23hingu@gmail.com>
2023-04-09 10:19:55 +0530

    challege2 complete

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res) {
-  res.send("success");
+  res.render("home",{homeContent:homeStartingContent});
 })
 
 
