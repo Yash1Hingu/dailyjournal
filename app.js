2f1bb4925756aa5b65257ea64d5f38b557e45f96
Yash1Hingu <yash23hingu@gmail.com>
2023-04-10 11:50:11 +0530

    remove lodash module and imporve post object

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const lodash = require("lodash/string");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -36,7 +35,7 @@ app.get("/posts/:topic",function(req,res) {
   const topicName = req.params.topic;
   let found = false;
   posts.forEach(function(post){
-    const postTitle = post.routetitle;
+    const postTitle = post.title;
     if(topicName === postTitle){
       res.render("post",{blogTitle : post.title,blogContent : post.text});
       found = true;
@@ -46,14 +45,12 @@ app.get("/posts/:topic",function(req,res) {
   if(!found) {
     res.render("post",{blogTitle : "404",blogContent : "Page Not Found"});
   }
-  
 })
 
 app.post("/compose",function(req,res) {
   const post = {
     title : req.body.blogTitle,
     text : req.body.blogText,
-    routetitle : lodash.kebabCase(req.body.blogTitle)
   }
   posts.push(post);
   res.redirect("/");
