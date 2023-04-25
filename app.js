344d12a2d4abf003a07a8eec73b93413f2ef998c
Yash1Hingu <yash23hingu@gmail.com>
2023-04-25 22:39:50 +0530

    Connect the Local MongoDB Server

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongooes = require("mongoose");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts = [];
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,7 +15,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res) {
+main().catch(err => console.log(err));
+async function main(){
+  await mongooes.connect("mongodb://127.0.0.1:27017/blogDB");
+}
+
+const postSchema = new mongooes.Schema({
+  title: String,
+  content: String
+})
+
+const Post = mongooes.model('Post',postSchema);
+
+app.get("/",async function(req,res) {
+  const posts = await Post.find();
   res.render("home",{homeContent:homeStartingContent,posts:posts});
 })
 
@@ -31,28 +44,22 @@ app.get("/compose",function(req,res) {
   res.render("compose");
 })
 
-app.get("/posts/:topic",function(req,res) {
-  const topicName = req.params.topic;
-  let found = false;
-  posts.forEach(function(post){
-    const postTitle = post.title;
-    if(topicName === postTitle){
-      res.render("post",{blogTitle : post.title,blogContent : post.text});
-      found = true;
-    } 
-  })
-  
-  if(!found) {
-    res.render("post",{blogTitle : "404",blogContent : "Page Not Found"});
-  }
+app.get("/posts/:id", async function(req,res) {
+  const topicid = req.params.id;
+  await Post.findById(topicid).then((post)=>{
+    res.render("post",{blogTitle: post.title,blogContent: post.content})
+  }).catch(()=>{
+      res.render("post",{blogTitle : "404",blogContent : "Page Not Found"});
+  });
+
 })
 
-app.post("/compose",function(req,res) {
-  const post = {
+app.post("/compose",async function(req,res) {
+  const post = new Post({
     title : req.body.blogTitle,
-    text : req.body.blogText,
-  }
-  posts.push(post);
+    content : req.body.blogText,
+  });
+  await post.save();
   res.redirect("/");
 })
 
